c55fd5f98584a25df9e57824d36fca444a307762
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-21 17:09:43 -0500

    scoping in practice

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `You, ${firstName}, are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = 'Bob';
+      const str = `Oh, and you're a millenial, ${firstName}, ${age}`;
+
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // add(2, 3);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Brandon';
+
+calcAge(1984);
