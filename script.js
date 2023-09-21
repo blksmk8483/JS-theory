b5bb06f9abc2e28856a78117ca043a174fac27bc
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-21 18:14:34 -0500

    more scoping theory practice:

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
   function printAge() {
-    const output = `You, ${firstName}, are ${age}, born in ${birthYear}`;
+    let output = `You, ${firstName}, are ${age}, born in ${birthYear}`;
     console.log(output);
 
     if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
       const firstName = 'Bob';
       const str = `Oh, and you're a millenial, ${firstName}, ${age}`;
 
+      output = 'NEW OUTPUT!';
       console.log(str);
 
       function add(a, b) {
@@ -18,6 +20,8 @@ function calcAge(birthYear) {
       }
     }
     // add(2, 3);
+    console.log(millenial);
+    console.log(output);
   }
   printAge();
   return age;
