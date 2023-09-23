931358b4984b396a8e4df8563e5c6e3436ac0467
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-22 23:25:50 -0500

    hoisting practice

--- script.js
+++ script.js
@@ -30,3 +30,40 @@ function calcAge(birthYear) {
 const firstName = 'Brandon';
 
 calcAge(1984);
+
+// Hoisting with variables....
+console.log(me);
+// console.log(job);
+// console.log(year);
+var me = 'Brandon';
+let job = 'teacher';
+const year = 1984;
+
+// Functions
+console.log(addDec(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDec(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b;
+
+// Example
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+// Example 2
+var x = 1;
+let y = 2;
+const z = 3;
