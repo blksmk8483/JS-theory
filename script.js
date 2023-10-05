f4e3653ea2e88bbacd330fbb52fb21b056198971
Brandon Knight <knightbrandon1984@gmail.com>
2023-10-04 21:45:12 -0500

    maps

--- script.js
+++ script.js
@@ -975,3 +975,38 @@ console.log(
 );
 
 console.log(new Set('brandonknight').size);
+
+// ----------------
+// ----------------
+// ----------------
+// ----------------
+console.log('--------MAPS--------');
+console.log('--------MAPS--------');
+
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open 😁')
+  .set(false, 'We are closed 🙁');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+const time = 21;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+console.log(rest.has('categories'));
+rest.delete(2);
+
+const arr = [1, 2];
+rest.set(arr, 'Test');
+console.log(rest);
+console.log(rest.size);
+
+console.log(rest.get(arr));
