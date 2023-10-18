9e543eb07051ad278601b8cedbaa50a162b8abd0
Brandon Knight <knightbrandon1984@gmail.com>
2023-10-18 16:39:52 -0500

    challenge #4 complete

--- script.js
+++ script.js
@@ -1298,3 +1298,64 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(15);
 planesInLine(3);
+
+// ------------------------
+// ------------------------
+// ------------------------
+// ------------------------
+// ------------------------
+console.log('============================');
+console.log('============================');
+console.log('============================');
+console.log('CHALLENGE #4');
+
+// Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+// The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+// Test data (pasted to textarea, including spaces):
+// underscore_case
+//  first_name
+// Some_Variable
+//   calculate_AGE
+// delayed_departure
+// Should produce this output (5 separate console.log outputs): underscoreCase ✅
+// firstName ✅
+// someVariable ✅
+// calculateAge ✅
+// delayedDeparture ✅
+// Hints:
+// § Remember which character defines a new line in the textarea 😉
+// § The solution only needs to work for a variable made out of 2 words, like a_b
+// § Start without worrying about the ✅. Tackle that only after you have the variable
+// name conversion working 😉
+// § This challenge is difficult on purpose, so start watching the solution in case
+// you're stuck. Then pause and continue!
+// Afterwards, test with your own test data! GOOD LUCK 😀
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)}${'✅'.repeat(i + 1)}`);
+  }
+});
+
+// const nameConverter = function (name) {
+//   const names = name.toLowerCase().split('_');
+//   const namesUpper = [];
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(''), '✅');
+// };
+
+// nameConverter('underscore_case');
